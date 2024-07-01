3dd451593c6464771babdd8f339859aa3a135f75
Lorenzo Amici <lorenzo.amici@outlook.com>
2024-07-01 11:48:44 +0200

    custom css

--- conf.py
+++ conf.py
@@ -71,6 +71,9 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = [r"html\_static"]
+html_css_files = [
+    'css/custom.css',
+]
 
 # code for making links open in a new tab
 
